354038823dcc25ca5cb2ae9ae65701937f188e4f
mistyxcutie <mahimabisht2615@gmail.com>
2022-09-19 20:56:45 +0530

    commit

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var counter = 0;
 
 function jump() {
-    if (character.classList != "animate") {
-        character.classList.add("animate");
-    }
+    if (character.classList == "animate") { return }
+    character.classList.add("animate");
     setTimeout(function() {
         character.classList.remove("animate");
-    }, 500); //here 500 means after 500m s animation will stop
+    }, 300); //here 300 means after 500m s animation will stop
 }
 
 var checkDead = setInterval(function() {
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 20 && blockLeft > 0 % % characterTop >= 130) {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
         block.style.animation = "none";
-        block.style.display = "none";
-        alert("YOU LOSE!!!! AHAHAHA!!");
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
+        block.style.animation = "block 1s infinite linear";
+    } else {
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 
 }, 10);
